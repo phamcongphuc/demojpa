836e66c9257aeb25ba829ce26c3c7b84777f4962
phamcongphuc <phamcongphuc32@gmail.com>
2023-02-14 20:52:18 +0700

    TK666

--- src/main/java/com/cyber/demojpa/controller/loginController.java
+++ src/main/java/com/cyber/demojpa/controller/loginController.java
@@ -1,5 +1,7 @@
 package com.cyber.demojpa.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
